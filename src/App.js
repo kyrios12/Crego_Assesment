95e131f3f1a4620e82d5fd953f51dd1a8102fdfb
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2024-01-25 19:10:08 +0530

    Installing required modules and setting up basic form and input types

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { Button, Form, Dropdown, Container, Row, Col} from 'react-bootstrap';
+import { useState} from 'react';
+
+export default function ExpressionForm(){
+  const initialState = {
+    connectorType: 'AND',
+    expressions: [{ ruleType: 'Age', operator: '>=', value: '', score: '' }],
+  };
+  const [state, setState] = useState(initialState);
+  return (
+    <Container >
+      <h1>Expression Engine</h1>
+      <Form onSubmit={""}>
+        <Form.Group controlId="connectorType">
+          <Form.Label>Connector Type</Form.Label>
+          <Form.Control as="select" onChange={""} value={""} required>
+            <option value="AND">AND</option>
+            <option value="OR">OR</option>
+          </Form.Control>
+        </Form.Group>
+
+        
+          <div>
+            <Row>
+              <Col>
+                <Form.Group controlId={""}>
+                  <Form.Label>Rule Type</Form.Label>
+                  <Dropdown onSelect={""}>
+                    <Dropdown.Toggle variant="success" id={""}>
+                      
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item eventKey="Age">Age</Dropdown.Item>
+                      <Dropdown.Item eventKey="Credit Score">Credit Score</Dropdown.Item>
+                      <Dropdown.Item eventKey="Account Balance">Account Balance</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group controlId={""}>
+                  <Form.Label>Operator</Form.Label>
+                  <Form.Control as="select" value={""} onChange={""}>
+                    <option value=">">{'>'}</option>
+                    <option value="<">{'<'}</option>
+                    <option value=">=">{'>='}</option>
+                    <option value="<=">{'<='}</option>
+                    <option value="=">{'='}</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group controlId={""}>
+                  <Form.Label>Value</Form.Label>
+                  <Form.Control type="text" value={""}  onChange={""} />
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group controlId={""}>
+                  <Form.Label>Score</Form.Label>
+                  <Form.Control type="text" value={""} onChange={""} />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Button variant="danger" onClick={""}>
+              Delete Expression
+            </Button>
+          </div>
+        
+
+        <Button variant="primary" onClick={""} >
+          Add Expression
+        </Button>
+
+        <Button variant="success" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ExpressionForm from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ExpressionForm />
+  </React.StrictMode>
+);
